ac2dca0e45344072359b58c08e43b50aaab30e85
AndrewsLR <48866053+AndrewsLR@users.noreply.github.com>
2020-11-14 22:39:16 -0300

    Add files via upload

--- app/Http/Controllers/ONGController.php
+++ app/Http/Controllers/ONGController.php
@@ -4,12 +4,14 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\DB;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 use Auth;
+use App\Models\Ong_log;
 
 
 class ONGController extends Controller
 {
+		
     public function registro(Request $request) {
 		
 		$result=DB::insert("insert into ongs(name,email,phone,address,password) values(?,?,?,?,?)",
@@ -21,7 +23,7 @@ class ONGController extends Controller
 		return redirect('ong_login')->with('message','registro realizado com sucesso');
 	}
 	
-	function login(Request $request){
+	public function login(Request $request){
 		
 			
 		$Reg_User =DB::select('select * from ongs where email = :email',[$request->get('email')]);
@@ -29,11 +31,41 @@ class ONGController extends Controller
 			return back()->with('message', 'dados de login incorretos');
 		else{
 			if ($Reg_User[0]->password == $request->get('senha')){
-				return redirect('ongs_mng')->with('ONG',$Reg_User[0]->name);
+				session()->put('ONG',$Reg_User[0]);
+				return redirect('ongs_mng');
 			}
 			else
 				return back()->with('message', 'dados de login incorretos');
 		}
+	}
+	
+	public function check_reg(Request $request){
+		if($request->input('name') == NULL
+					||
+		$request->input('email') == NULL
+					||
+		$request->input('fone') == NULL
+					||
+		$request->input('endr') == NULL
+					||
+		$request->input('senha') == NULL)
+		return back()->with('message', 'Preencha todos os dados');
+		
+		else
+			ONGController::registro($request);
+			
+	}
+	
+	static public function check_ongs(){
+		if (session()->has('ONG'))
+			redirect('ongs_mng');
+		else
+			return back();
+	}
+	
+	public function logout(){
+		session()->forget('ONG');
+		return redirect('ong_login');
 	}
 	
 }
